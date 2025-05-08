dddbd16e500c0fc227494be27b5735764a5fd218
Dmitry <dmitry.oborsky@gmail.com>
2025-05-08 18:02:38 +0300

    added jwt validation at Auth endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,17 +9,31 @@ export class AuthService {
     private readonly prisma: PrismaService,
   ) {}
 
-  async login() {
-    const userId = (await this.prisma.user.create({})).uid
-    const payload = {
-      role: 'guest',
-      anonId: userId,
-      secret: process.env.JWT_SECRET,
+  async login(access_token?: string) {
+    const createToken = async () => {
+      const uid = (await this.prisma.user.create({})).uid
+      const payload = {
+        role: 'guest',
+        anonId: uid,
+        secret: process.env.JWT_SECRET,
+      }
+
+      return {
+        access_token: this.jwtService.sign(payload),
+        uid,
+      }
     }
 
-    return {
-      access_token: this.jwtService.sign(payload),
-      userId,
+    if (!access_token) return await createToken()
+
+    try {
+      const tokenData = this.jwtService.verify(access_token)
+      return {
+        uid: tokenData.anonId,
+        access_token,
+      }
+    } catch (error) {
+      return await createToken()
     }
   }
 }
